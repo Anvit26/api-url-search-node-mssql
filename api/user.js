47345b066f8c486043eb27e1a76bb15b4679b266
Anvit26 <57208745+Anvit26@users.noreply.github.com>
2019-12-25 13:44:42 +0530

    Update user.js

--- api/user.js
+++ api/user.js
@@ -4,7 +4,7 @@ const route = require('express').Router();
 route.get('/', (req, res) => {
     User.findAll()
         .then((userinfos) => {
-            res.status(200).send(userinfos)
+            res.status(200).send(userinfos  )
         })
         .catch((err) => {
             res.status(500).send({
@@ -14,65 +14,52 @@ route.get('/', (req, res) => {
 })
 //UserId
 route.get('/UserId/:UserId', (req, res) => {
-    let UserId = req.params.UserId;
-    console.log(req.params);
-    User.findOne({ where: { UserId: req.params } })
+    User.findAll({ where: { UserId: req.params.UserId } })
         .then((userinfos) => {
             res.status(200).send(userinfos)
-            console.log(res)
         })
         .catch((err) => {
-            console.log(res)
             res.status(500).send({
                 error: "could not retrive User"
             })
         })
-});
+})
 //Name
 route.get('/Name/:Name', (req, res) => {
-    let Name = req.params.Name;
-    console.log(req.params);
-    User.findAll({ where: { Name: req.params } })
+    User.findAll({ where: { Name: req.params.Name } })
         .then((userinfos) => {
             res.status(200).send(userinfos)
         })
-        .catch(() => {
-            console.log(res)
+        .catch((err) => {
             res.status(500).send({
                 error: "could not retrive User"
             })
         })
-});
+})
 //Age
 route.get('/Age/:Age', (req, res) => {
-    let Age = req.params.Age;
-    console.log(req.params);
-    User.findAll({ where: { Age: req.params } })
-        .then((userinfos) => {
-            res.status(200).send(userinfos)
-        })
-        .catch(() => {
-            console.log(res)
-            res.status(500).send({
-                error: "could not retrive User"
+        User.findAll({ where: { Age: req.params.Age} })
+            .then((userinfos) => {
+                res.status(200).send(userinfos)
             })
-        })
-});
+            .catch((err) => {
+                res.status(500).send({
+                    error: "could not retrive User"
+                })
+            })
+    })
 //Mobile
 route.get('/Mobile/:Mobile', (req, res) => {
-    let Mobile = req.params.Mobile;
-    console.log(req.params);
-    User.findAll({ where: { Mobile: req.params } })
+    User.findAll({ where: { Mobile: req.params.Mobile } })
         .then((userinfos) => {
             res.status(200).send(userinfos)
         })
-        .catch(() => {
-            console.log(res)
+        .catch((err) => {
             res.status(500).send({
                 error: "could not retrive User"
             })
         })
-});
+})
 //Post
 route.post('/post', (req, res) => {
     User.create({
@@ -96,4 +83,23 @@ route.post('/post', (req, res) => {
 //    var qp = req.query;
 //    res.json(qp);
 //});
+
+/*
+ //err
+ route.get('/UserId/:UserId', (req, res) => {
+
+    console.log(req.params);
+    User.findOne({ where: { UserId: req.params } })
+        .then((userinfos) => {
+            res.status(200).send(userinfos)
+            console.log(res)
+        })
+        .catch((err) => {
+            console.log(res)
+            res.status(500).send({
+                error: "could not retrive User"
+            })
+        })
+});*/
+
 exports = module.exports = route
