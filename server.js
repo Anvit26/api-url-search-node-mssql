e1cdc88f6b1a7ad32426a0c5b5d771623205c0cc
Anvit26 <57208745+Anvit26@users.noreply.github.com>
2020-01-24 14:11:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const Sequelize = require("sequelize");
 const path = require("path");
+const cors = require('cors');
 
 const app = express();
 //middleware    
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors());
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 //routes
 app.use('/', express.static(path.join(__dirname, 'public')))
 app.use('/api', require('./api').route)
